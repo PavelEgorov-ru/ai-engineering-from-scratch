3a1ecb39ab1aa946601a6654aaa2f5085571af0f
PavelEgorov-ru <pkotofffff@yandex.ru>
2026-05-18 08:26:27 +0300

    промежуток урока 7 фазы 0

---
+++ scratch/lesson-07/app/main.py
@@ -0,0 +1,16 @@
+import socket
+import sys
+
+from rich.console import Console
+from rich.panel import Panel
+
+
+def main() -> None:
+    console = Console()
+    message = f"""Привет из твоего первгого контейнера!
+У тебя установлен python - {sys.version} на компьютере - {socket.gethostname()}"""
+    console.print(Panel(message))
+
+
+if __name__ == "__main__":
+    main()
